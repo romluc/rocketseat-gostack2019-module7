232b247fdbc00e1fd865d554d70dd40c30efac7b
romluc <romulofilhofilho@gmail.com>
2019-09-20 20:36:03 -0300

    Create and style Order component

--- src/components/Order/index.js
+++ src/components/Order/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Container, Text } from './styles';
+import { Container, Text, Separator } from './styles';
 
 export default function Order() {
   return (
     <Container>
+      <Text>Your order has been placed</Text>
+      <Separator />
       <Text>Thank you for shopping with us!</Text>
     </Container>
   );
--- src/components/Order/styles.js
+++ src/components/Order/styles.js
@@ -3,8 +3,15 @@ import styled from 'styled-components';
 export const Container = styled.div`
   display: flex;
   flex: 1;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
+`;
+
+export const Separator = styled.hr`
+  color: #777;
+  width: 80%;
+  margin: 5px 10px 5px;
 `;
 
 export const Text = styled.h1`
